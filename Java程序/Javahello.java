e52df0b60230ee882171a6981207927b89de8a80
theoneofc <cocoprwh@gmail.com>
2020-04-28 03:29:13 +0800

    new a repository and add files

---
+++ Java程序/Javahello.java
@@ -0,0 +1,11 @@
+public class Javahello{
+	public static void main(string[] args){
+	String s;
+	s = "Hello ,Java!";
+	system.out.println(s)
+	//system.out.println("Hello ,Java!")
+
+	    double pi = 3.14159
+	    System.out.println("%.2f\n",d);
+	}
+}
---
+++ Java程序/java基础学习.cpp
@@ -0,0 +1,31 @@
+//if 
+public class Main {
+    public static void main(String[] args) {
+        int n = 90;
+        if (n > 90) {
+            System.out.println("优秀");
+        } else if (n >= 60) {
+            System.out.println("及格了");
+        } else {
+            System.out.println("挂科了");
+        }
+    }
+}
+
+//switch 
+option = 输入选择
+	switch（option）{
+	case 1:
+		功能接口，例如开始；
+		break;
+	case 2:
+		 功能接口，例如设置；
+		 break;
+	... 
+	default://其他所有情况 ，缺失，就是没有符合选择的意思 
+        System.out.println("Not selected");
+        break;
+	}
+	
+
+
